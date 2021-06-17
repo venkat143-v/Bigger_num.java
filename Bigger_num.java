f85e2b5a2d764925b55f8ae2b63321944f7dc5dd
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-17 17:01:49 +0530

    Add files via upload

---
+++ Bigger_num.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Bigger_num {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers :");
+		int a = sc.nextInt(); 
+		int b = sc.nextInt();		
+if(a>b)	
+System.out.println("bigger number = "+ a);
+else
+System.out.println("bigger number = "+ b);
+	}
+}
